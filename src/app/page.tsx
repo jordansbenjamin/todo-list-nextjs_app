ca35ab73e5e03f5bdeb0a08df75f6b5b9e84c649
jordansbenjamin <14327@coderacademy.edu.au>
2023-10-27 22:32:45 +1100

    Added new page route and TodoItem component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
-export default function Home() {
-	return <h1>Hi</h1>;
+import prisma from "@/db";
+import Link from "next/link";
+import { TodoItem } from "@/components/Todoitem";
+
+function getTodos() {
+	return prisma.todo.findMany();
+}
+
+export default async function Home() {
+	const todos = await getTodos();
+	// await prisma.todo.create({ data: { title: "test", complete: false } });
+
+	return (
+		<>
+			<header className="flex justify-between items-center mb-4">
+				<h1 className="text-2xl">Todos</h1>
+				<Link
+					className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+					href="/new">
+					New
+				</Link>
+			</header>
+			<ul className="pl-4">
+				{todos.map((todo) => (
+					// <li key={todo.id}>{todo.title}</li>
+					<TodoItem key={todo.id} {...todo} />
+				))}
+			</ul>
+		</>
+	);
 }
